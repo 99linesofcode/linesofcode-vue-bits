d931ebd5fd93e7222952b53603662b83a93d162f
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2020-09-22 23:04:37 +0200

    :sparkles: Add entrypoint for webpack-dev-server

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <form-group label="Hello world" />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { FormGroup } from '@/main';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    FormGroup,
+  }
+});
+</script>
